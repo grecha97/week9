b6fa173750ef68846be69f985ca8307270c40cb2
grechkina97@gmail.com <grecha97>
2024-02-08 17:04:39 +0800

    fix deposit file

--- deposit.js
+++ deposit.js
@@ -1,3 +1,4 @@
+const sum = document.querySelector('.sum').value;
 const button = document.querySelector('.button');
 const total = document.querySelector('.total');
 
@@ -7,16 +8,12 @@ const deposit = '30000';
 
 const growth = '150';
 
-const currency = 'руб';
+const currency = 'руб.';
 
 button.addEventListener('click', () => {
   const sum = document.querySelector('.sum').value;
-  total.textContent = (message);// Через год у вас будет XXX руб. на счету", где вместо ХХХ выведите сколько получится на счету с учетом процентной ставки 7% годовых
-});
+  total.textContent = `Через год у вас будет ${sum * 0.07 + Number(sum) + currency}. на счету`;});
 
-console.log ("Ваш депозит на начало расчётного периода составлял " + deposit +  currency);
-console.log ("Согласно вашему тарифу, вам была присвоена ставка " + interestRate + "%");
-console.log ("К концу расчётного периода прирост составил " + growth + currency + ", и на данный момент ваш депозит составляет " + (Number(deposit) + Number(growth)));
-
-
-
+console.log (`Ваш депозит на начало расчётного периода составлял ${deposit} ${currency}`);
+console.log (`Согласно вашему тарифу, вам была присвоена ставка ${interestRate} %`);
+console.log (`К концу расчётного периода прирост составил ${growth} ${currency}, и на данный момент ваш депозит составляет ${(Number(deposit) + Number(growth))} ${currency}`);
